f95a9f95254e6572a755d1737e3b32943411ded2
FilipStaahl <filip.stahl@student.forsbergsskola.se>
2023-10-09 15:39:11 +0200

    p1 with arrays added

--- P1Classes_And_Objects/Program.cs
+++ P1Classes_And_Objects/Program.cs
@@ -1,4 +1,6 @@
-﻿Person printPerson = new Person();
+﻿//P01.1
+/*
+Person printPerson = new Person();
 Console.WriteLine(printPerson);
 Animal printAnimal = new Animal();
 Console.WriteLine(printAnimal);
@@ -15,6 +17,19 @@ public class Animal
 }
 
 public class Car
+{
+    
+}
+*/
+
+Person[] people = new Person[3];
+
+for (int i = 0; i < people.Length; i++)
+{
+    people[i] = new Person();
+    Console.WriteLine(people[i]);
+}
+public class Person
 {
     
 }
