38b2cc6bea32c8edd23c6ba6c16f339de3fdeb7c
FilipStaahl <filip.stahl@student.forsbergsskola.se>
2023-10-10 13:12:26 +0200

    P2_2ItemValues finished

--- P2Fields/Player.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace P2Fields;
-
-public class Player
-{
-    
-}
--- P2Fields/Program.cs
+++ P2Fields/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿public class Profile
+{
+    public int gold;
+    public int stones;
+    public int wood;
+    public int level;
+    public int experience;
+    public bool subscription;
+    public bool pvp;
+    public string displayName;
+    public string name;
+}
